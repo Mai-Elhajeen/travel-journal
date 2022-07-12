b680228ce30f786b2f367d23dfecbf2471613269
Mai-ELhajeen <maielhajeen2001@gmail.com>
2022-07-12 15:14:38 +0300

    adding style for header ;

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Header() {
   return (
-      <header>
+      <header className='header'>
         <i class="ri-earth-fill"></i>
         <p>my travel journal.</p>
       </header>
