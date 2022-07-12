35191fdc5eb6b4f869c351fa502d9b8d35d3c7e3
Mai-ELhajeen <maielhajeen2001@gmail.com>
2022-07-12 15:40:34 +0300

    calling separate file in app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import Header from './components/Header'
 import Main from './components/Main'
+import separate from './separate.js'
+const travels = separate.map(travel => {
+  return (
+    <Main />
+  )
+})
+
 function App() {
   return (
     <div className="container">
       <Header />
-      <Main />
+      {travels}
     </div>
   );
 }
--- src/separate.js
+++ src/separate.js
@@ -30,3 +30,4 @@ export default [
         imgURL: './img/Norway.jpg'
     }
 ]
+
